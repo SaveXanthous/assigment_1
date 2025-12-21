6862e25c03911442ae16d3181317fc7837a96760
SaveXanthous <savexanthous@gmail.com>
2025-12-21 20:39:23 +0500

    release: v1.0

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,148 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+void main(){
+    final int countOfStudent = 5;
+    final String[]  name_students = {"Nikita","Sasha","Vova","Masha","Katya"};
+    final String[] major_of_students = {"CS","SE","CS","SE","SE"};
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    Course course = new Course("course_1", "Anton",countOfStudent);
+
+    for(int i = 0;i < countOfStudent;i++){
+        course.addStudent(new Student(name_students[i], i, major_of_students[i]),i);
+        course.students[i].updateGPA(Math.round(Math.random() * 5.0 * 10.0) / 10.0);
+        course.students[i].addCredits((int)(Math.random() * 28));
+    }
+
+    System.out.print(course);
+
+    System.out.print("Top Student:\n" + getTopStudent(course.students));
+    System.out.println("Count of honors students: " + countHonors(course.students));
+    System.out.println("Total credits: " + totalCredits(course.students));
+
+}
+
+public static Student getTopStudent(Student[] arr){
+    Student student = arr[0];
+    for(int i = 1; i<arr.length; i++){
+        if(arr[i].getGpa() > student.getGpa()){
+            student = arr[i];
+        }
+    }
+    return student;
+}
+
+public static int countHonors(Student[] arr){
+    int count = 0;
+    for(Student student: arr){
+        if(student.isHonors()){
+            count++;
+        }
+    }
+    return count;
+}
+
+public static int totalCredits(Student[] arr){
+    int count = 0;
+    for(Student student: arr){
+        count += student.getCredits();
+    }
+    return count;
+}
+
+public class Student{
+    private String name;
+    private int id;
+    private String major;
+    private double gpa;
+    private int credits;
+
+    public Student(String name,int id,String major){
+        this.name = name;
+        this.id = id;
+        this.major = major;
+        this.gpa = 0.0;
+        credits = 0;
+    }
+
+
+
+    public String getName(){
+        return this.name;
+    }
+    public String getMajor(){
+        return this.major;
+    }
+    public int getCredits(){
+        return this.credits;
+    }
+    public int getId(){
+        return this.id;
+    }
+    public double getGpa(){
+        return this.gpa;
+    }
+
+    public void addCredits(int credits){
+        this.credits += credits;
+    }
+    public void updateGPA(double newGPA){
+        this.gpa = newGPA;
+    }
+    public boolean isHonors(){
+        return this.gpa >= 3.5;
+    }
+
+    @Override
+    public String toString(){
+        return "name: " + name + " id: " + id + " major: " + major + " gpa: " + gpa + " credits: " + credits + "\n";
+    }
+}
+
+public class Course{
+    private String courseName;
+    private String instructor;
+    private Student[] students;
+
+    public Course(String courseName, String instructor, int size){
+        this.courseName = courseName;
+        this.instructor = instructor;
+        this.students = new Student[size];
+    }
+
+    public void addStudent(Student s, int index){
+        this.students[index] = s;
+    }
+
+    // it is the strangest function
+    // it mustn't return gpa or to save it
+    public void courseAverageGPA(){
+        double gpa = 0;
+        for(Student student: students){
+            gpa += student.getGpa();
+        }
+        gpa /= students.length;
+    }
+
+    public Student highestCreditStudent(){
+        Student maxCredit = students[0];
+        for(int i = 1; i<students.length; i++){
+            if(students[i].getCredits() > maxCredit.getCredits()){
+                maxCredit = students[i];
+            }
+        }
+        return maxCredit;
+    }
+
+    @Override
+    public String toString(){
+        String c = "";
+
+        c += "Course name: " +  courseName + "\n";
+        c += "Instructor name: " +  instructor + "\n";
+        c += "Students: \n";
+
+        for(Student student : students){
+            c += student;
+        }
+
+        return c;
     }
 }
